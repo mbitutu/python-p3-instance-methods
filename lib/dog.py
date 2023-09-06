717f82d1444d4c34328d2dff11f4fab537933162
Bitutu <maryanne.osoro@student.moringaschool.com>
2023-09-06 09:59:31 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
+        
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("sit")
+
+fido = Dog()
+fido.bark()
+
+
+fido.sit()
